8406ae08ec59222cb061b23ef9265cff4f856ba4
Kashif1Naqvi <mrkashif029@gmail.com>
2020-04-06 16:23:10 +0500

    initial commit

---
+++ portfolio/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+class Project(models.Model):
+  title = models.CharField(max_length=100)
+  description = models.CharField(max_length=300)
+  image  = models.ImageField(upload_to='portfolio/images')
+  url   = models.URLField(blank = True)
