532f749d25db34a644071701fe3b310e9cf94f2d
Kashif1Naqvi <mrkashif029@gmail.com>
2020-04-06 22:01:46 +0500

    code updated

--- django_portfolio_project/urls.py
+++ django_portfolio_project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
+from portfolio import views
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name='home')
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Project
+def home(request):
+  posts = Project.objects.all()
+  # posts = [
+  #   {
+  #     'name':"kashif",
+  #     'section':"bscs"
+  #   }
+  # ]
+  return render(request,'portfolio/home.html',{'posts':posts})
