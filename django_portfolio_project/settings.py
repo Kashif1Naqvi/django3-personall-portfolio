38980b93d2de77a53177d3137590bb77a43e3fa3
Kashif1Naqvi <mrkashif029@gmail.com>
2020-04-10 22:47:24 +0500

    code updated

--- django_portfolio_project/settings.py
+++ django_portfolio_project/settings.py
@@ -125,7 +125,6 @@ STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 MEDIA_URL  = '/media/'
 
-
 try:
     from .local_settings import *
 except ImportError:
