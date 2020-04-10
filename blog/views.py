8cb8cfe38762e38a71ec61124b7e7ac4663b661b
Kashif1Naqvi <mrkashif029@gmail.com>
2020-04-10 11:05:41 +0500

    code updated

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Post
+def all_blogs(request):
+    posts = Post.objects.order_by('-date')[:5]
+    return render(request,'blog/all_blogs.html',{'posts':posts})
 
-# Create your views here.
+
+def detail(request,blog_id):
+    blog = get_object_or_404(Post,pk=blog_id)
+    return render(request,'blog/detail.html',{'blog':blog})
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
 from .models import Project
 def home(request):
-  posts = Project.objects.all()
-  # posts = [
-  #   {
-  #     'name':"kashif",
-  #     'section':"bscs"
-  #   }
-  # ]
-  return render(request,'portfolio/home.html',{'posts':posts})
+  projects = Project.objects.all()
+  return render(request,'portfolio/home.html',{'projects':projects})
